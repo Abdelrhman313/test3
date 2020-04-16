2aba5ad9ecff7c6c275f9b77499e9a2a05cf9c67
Abdelrhman <58594875+Abdelrhman313@users.noreply.github.com>
2020-04-16 19:31:09 +0200

    update index.php

--- index.php
+++ index.php
@@ -1,1 +1,264 @@
+<!DOCTYPE html>
 
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Responsive Web</title>
+    <link rel="stylesheet" href="Css/normalize.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="Css/main.css" />
+    <link href="https://fonts.googleapis.com/css?family=Coda&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Start Header-->
+    <div class="header text-center">
+        <div class="overlay">
+            <div class="container">
+                <h1>Choose Your Plan</h1>
+                <div class="plan float-left">Free Plan Equal 10GB</div>
+                <div class="plan float-left">10$ Plan Equal 20GB</div>
+                <div class="clearfix"></div>
+                <div class="order">Order Now And Get Other Plan For Free</div>
+            </div>
+        </div>
+    </div>
+    <!-- End Header-->
+
+    <!--Start Navbar-->
+    <div class="navbar">
+        <div class="navbar-header float-left">
+            <a href="#" class="nav-brand float-left">BoDy</a>
+            <i class="fa fa-bars visible-xs float-left"></i>
+        </div>
+        <div class="navbar-menu float-left">
+            <ul class="list-unstyled menu-list">
+                <li class="current  float-left">Home</li>
+                <li class="float-left">Features</li>
+                <li class="float-left">FAQ</li>
+                <li class="float-left" style="position: relative">Products <i class="fa fa-arrow-down"></i>
+                    <ul class="list-unstyled dropdown">
+                        <li>Prouduct 1</li>
+                        <li>Prouduct 2</li>
+                        <li>Prouduct 3</li>
+                        <hr />
+                        <li>Other Product</li>
+                    </ul>
+                </li>
+                <li class="float-left">About</li>
+                <li class="float-left">Contact</li>
+            </ul>
+        </div>
+    </div>
+    <!--End Navbar-->
+
+    <!--start Features-->
+    <div class="features text-center">
+        <h2 class="h1">Our Features</h2>
+        <div class="container">
+            <div class="feat float-left padding">
+                <h2>Header</h2>
+                <p>This Is Paragraph This Is Paragraph This Is Paragraph This Is Paragraph This Is Paragraph.</p>
+            </div>
+            <div class="feat float-left padding">
+                <h2>Header</h2>
+                <p>This Is Paragraph This Is Paragraph This Is Paragraph This Is Paragraph This Is Paragraph.</p>
+            </div>
+            <div class="feat float-left padding">
+                <h2>Header</h2>
+                <p>This Is Paragraph This Is Paragraph This Is Paragraph This Is Paragraph This Is Paragraph.</p>
+            </div>
+            <div class="feat float-left padding">
+                <h2>Header</h2>
+                <p>This Is Paragraph This Is Paragraph This Is Paragraph This Is Paragraph This Is Paragraph.</p>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <!--start Features-->
+
+    <!--Start About Us-->
+    <div class="about-us">
+        <div class="container">
+            <h2 class="h1 text-center">About-Us</h2>
+            <p class="res_p">Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum
+                dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum
+                dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax.</p>
+        </div>
+    </div>
+    <!--End About Us-->
+
+    <!--Start Our_Work-->
+    <div class="our_work">
+        <h2 class="h1 text-center">Our Work</h2>
+        <ul class="list-unstyled">
+            <li data-class=".all" class="active">All</li>
+            <li data-class=".website">Website</li>
+            <li data-class=".design">Design</li>
+            <li data-class=".markiting">Markiting</li>
+            <li data-class=".blog">Blog</li>
+            <li data-class=".responsive">Responsive</li>
+        </ul>
+        <div class="image float-left">
+            <img src="Images/1.jpg" alt="image" class="responsive-image website all">
+        </div>
+        <div class="image float-left">
+            <img src="Images/2.jpg" alt="image" class="responsive-image design all">
+        </div>
+        <div class="image float-left">
+            <img src="Images/3.jpg" alt="image" class="responsive-image markiting all">
+        </div>
+        <div class="image float-left">
+            <img src="Images/4.jpg" alt="image" class="responsive-image website all">
+        </div>
+        <div class="image float-left">
+            <img src="Images/5.jpg" alt="image" class="responsive-image responsive all">
+        </div>
+        <div class="image float-left">
+            <img src="Images/6.jpg" alt="image" class="responsive-image blog all">
+        </div>
+        <div class="image float-left">
+            <img src="Images/7.jpg" alt="image" class="responsive-image responsive all">
+        </div>
+        <div class="image float-left">
+            <img src="Images/8.jpg" alt="image" class="responsive-image design all">
+        </div>
+        <div class="clearfix"></div>
+    </div>
+    <!--End Our_Work-->
+
+    <!--Start our_prising-->
+    <div class="our_prising text-center">
+        <div class="container">
+            <h2 class="h1">Our Prising</h2>
+            <div class="prising_plan float-left plus">
+                <h2>Plus</h2>
+                <span class="btn-danger">10$</span>
+                <ul class="list-unstyled">
+                    <li>Disk Memory :20G</li>
+                    <li>BandWidth : 100G</li>
+                    <li>FTP Protocol : 5</li>
+                    <li>DataBase : 50G</li>
+                    <li>Free Domain</li>
+                </ul>
+                <button class="btn-danger">Buy Now</button>
+            </div>
+
+            <div class="prising_plan float-left primium">
+                <h2>Primium</h2>
+                <span class="btn-succes">20$</span>
+                <ul class="list-unstyled">
+                    <li>Disk Memory :40G</li>
+                    <li>BandWidth : 120G</li>
+                    <li>FTP Protocol : 8</li>
+                    <li>DataBase : 70G</li>
+                    <li>Free Domain</li>
+                </ul>
+                <button class="btn-succes">Buy Now</button>
+            </div>
+
+            <div class="prising_plan float-left unlimited">
+                <h2>Unlimited</h2>
+                <span class="btn-info">30$</span>
+                <ul class="list-unstyled">
+                    <li>Disk Memory :50G</li>
+                    <li>BandWidth : 140G</li>
+                    <li>FTP Protocol : 10</li>
+                    <li>DataBase : 80G</li>
+                    <li>Free Domain</li>
+                </ul>
+                <button class="btn-info">Buy Now</button>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <!--End our_prising-->
+
+    <!--Start Testimonial-->
+    <div class="testimonial">
+        <div class="container">
+            <h2 class="h1 text-center">What They Say About Us</h2>
+            <div class="person_say float-left">
+                <div class="avatar  float-left">
+                    <img src="Images/avatar_01.jpg" alt='image' class="responsive-image img-thumbnail" />
+                </div>
+                <h3 class="float-left">Ahmed</h3>
+                <p class="res_p float-left">Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit.</p>
+            </div>
+            <div class="person_say float-left">
+                <div class="avatar  float-left">
+                    <img src="Images/avatar_03.jpg" alt='image' class="responsive-image img-thumbnail" />
+                </div>
+                <h3 class="float-left">Somaya</h3>
+                <p class="res_p  float-left">Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit..</p>
+            </div>
+            <div class="person_say float-left">
+                <div class="avatar float-left">
+                    <img src="Images/avatar_04.jpg" alt='image' class="responsive-image img-thumbnail" />
+                </div>
+                <h3 class="float-left">Rehab</h3>
+                <p class="res_p float-left">Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit.</p>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <!--End Testimonial-->
+
+    <!--Start Information-->
+    <div class="information">
+        <div class="container">
+            <h2 class="h1 text-center">Product Information</h2>
+            <ul class="list-unstyled tab-list">
+                <li data-class=".about" class="selected">About</li>
+                <li data-class=".history">History</li>
+                <li data-class=".spec">Specification</li>
+                <li data-class=".technical">Technical</li>
+                <li data-class=".review">Review</li>
+            </ul>
+            <div class="info-content">
+                <div class="about">
+                    About Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit
+                    dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit
+                    dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax.
+                </div>
+                <div class="history">
+                    History Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit
+                    dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit
+                    dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax.
+                </div>
+                <div class="spec">
+                    Specification
+                    Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit
+                    dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit
+                    dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax.
+                </div>
+                <div class="technical">
+                    Technical
+                    Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit
+                    dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit
+                    dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax.
+                </div>
+                <div class="review">
+                    Review Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit
+                    dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit
+                    dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax Lorim Ipsum dolor emit dmsax.
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Start Information-->
+
+    <!--Start Footer-->
+    <div class="footer text-center">
+        <div class="copyright float-left">CopyRight &copy;2020 For FCIH.</div>
+        <div class="text float-left">Responsive Design Without Framework Designed By &hearts;</div>
+        <div class="clearfix"></div>
+    </div>
+    <!--End Footer-->
+
+    <script src="Js/jquery.js"></script>
+    <script src="Js/plugins.js"></script>
+</body>
+
+</html>
